f72c08ddcd947ff61ba187cdd9b1dca89557f9b9
hoangvqpedona <pedona.hoangvq@gmail.com>
2025-07-20 20:54:31 +0700

    ...

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,20 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/hoidanit")
+    public String getHomePage() {
+        return "eric.html";
+    }
+}
